3312753a0f072f7c55760c4293f662b1b05cd708
maciejkolanski <maciej.kolanski@gmail.com>
2016-03-15 23:28:34 +0100

    Przeslanie zmiennej gmp do slave

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <mpi/mpi.h>
+#include <gmp.h>
+#include <string.h>
 
 using namespace std;
 
-#define WORKTAG 1
+#define INITVALUETAG 1
 #define DIETAG 2
 
-constexpr int BUFFSIZE = 64;
-int buff[BUFFSIZE];
+constexpr int MAX_DIGITS = 64;
 
 static void master();
 static void slave();
@@ -15,11 +16,6 @@ static void slave();
 int main(int argc, char** argv)
 {
   int myrank;
-<<<<<<< HEAD
-  //MPI_Status status;
-=======
-  MPI_Status status;
->>>>>>> origin/master
 
   MPI_Init (&argc, &argv);      /* starts MPI */
   MPI_Comm_rank (MPI_COMM_WORLD, &myrank);        /* get current process id */
@@ -38,14 +34,24 @@ int main(int argc, char** argv)
   return 0;
 }
 
-static void master()
+void master()
 {
     int ntasks,buff;
-
     MPI_Comm_size(MPI_COMM_WORLD,&ntasks);
+
+    char valueStr[] = "124\0";
+    mpz_t value,step;
+    mpz_init(value);
+    mpz_init(step);
+    mpz_set_str(value,valueStr,10);
+
+    mpz_cdiv_q_ui(step,value,ntasks);
+    gmp_printf("MASTER value/tasksk = step %Zd/%d = %Zd \n",value,ntasks,step);
+
+
     for( int rank = 1; rank < ntasks; ++rank )
     {
-        MPI_Send(&buff,1,MPI_INT,rank,WORKTAG,MPI_COMM_WORLD);
+        MPI_Send(valueStr,strlen(valueStr),MPI_CHAR,rank,INITVALUETAG,MPI_COMM_WORLD);
     }
 
     for( int rank = 1; rank < ntasks; ++rank )
@@ -54,22 +60,29 @@ static void master()
     }
 }
 
-static void slave()
+void slave()
 {
-    int work,myrank;
+    MPI_Status status;
+    int myrank;
     MPI_Comm_rank (MPI_COMM_WORLD, &myrank);        /* get current process id */
 
-    MPI_Status status;
+    char valueStr[MAX_DIGITS];
+    mpz_t value;
+    mpz_init(value);
+
+    MPI_Recv(valueStr,MAX_DIGITS,MPI_CHAR,0, INITVALUETAG ,
+        MPI_COMM_WORLD, &status);
+    mpz_set_str(value,valueStr,10);
+    gmp_printf("Odebrano %Zd w procesie %d\n",value,myrank);
+
     for(;;)
     {
-        MPI_Recv(&work,1,MPI_INT,0, MPI_ANY_TAG,
+        MPI_Recv(valueStr,MAX_DIGITS,MPI_CHAR,0, MPI_ANY_TAG,
             MPI_COMM_WORLD, &status);
         if( status.MPI_TAG == DIETAG )
         {
             printf("Wysłano DIETAG do procesu %d\n",myrank);
             return;
         }
-        printf("Wysłano %d do procesu %d\n",work,myrank);
-
     }
 }
